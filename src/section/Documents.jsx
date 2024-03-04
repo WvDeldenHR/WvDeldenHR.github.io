05de08737d56c1de7984f0180dcd3e424faf4aa4
WvDeldenHR <0960572@hr.nl>
2024-03-04 18:07:19 +0100

    docs added

--- src/section/Documents.jsx
+++ src/section/Documents.jsx
@@ -1,9 +1,9 @@
 // Images
 import { ReactComponent as IconDownload } from "./../assets/icon/icon_download_01.svg";
 // Documents
-import documentFile01 from "./../assets/images/image_stock_phote_01.jpg";
-import documentFile02 from "./../assets/images/image_stock_phote_01.jpg";
-import documentFile03 from "./../assets/images/image_stock_phote_01.jpg";
+import documentFile01 from "./../assets/documents/onderzoek.docx";
+// import documentFile02 from "./../assets/images/image_stock_phote_01.jpg";
+// import documentFile03 from "./../assets/images/image_stock_phote_01.jpg";
 
 export function Documents() {
     return (
@@ -15,11 +15,11 @@ export function Documents() {
 
             <div className="table my-8">
                 <div className="table-row items-center w-full">
-                    <div className="table-cell border-b-[1px] border-white py-3 pl-3 w-full text-base font-light text-white">Bronnen Onderzoek</div>
-                    <a href={documentFile01} className="table-cell border-b-[1px] border-l-[1px] border-white py-3 pl-6 pr-3" download="Bronnen Onderzoek">
+                    <div className="table-cell border-b-[1px] border-white py-3 pl-3 w-full text-base font-light text-white">Onderzoek</div>
+                    <a href={documentFile01} className="table-cell border-b-[1px] border-l-[1px] border-white py-3 pl-6 pr-3" download="Onderzoek">
                         <IconDownload className="transition-fast | w-5 h-5 fill-white hover:fill-primary cursor-pointer" /></a>
                 </div>
-                <div className="table-row items-center w-full">
+                {/* <div className="table-row items-center w-full">
                     <div className="table-cell border-b-[1px] border-white py-3 pl-3 w-full text-base font-light text-white">Doelgroep Onderzoek</div>
                     <a href={documentFile02} className="table-cell border-b-[1px] border-l-[1px] border-white py-3 pl-6 pr-3" download="Doelgroep Onderzoek">
                         <IconDownload className="transition-fast | w-5 h-5 fill-white hover:fill-primary cursor-pointer" /></a>
@@ -28,7 +28,7 @@ export function Documents() {
                     <div className="table-cell py-3 pl-3 w-full text-base font-light text-white">Markt Onderzoek</div>
                     <div href={documentFile03} className="table-cell border-l-[1px] border-white py-3 pl-6 pr-3" download="Markt Onderzoek">
                         <IconDownload className="transition-fast | w-5 h-5 fill-white hover:fill-primary cursor-pointer" /></div>
-                </div>
+                </div> */}
             </div>
         </div>
     );
