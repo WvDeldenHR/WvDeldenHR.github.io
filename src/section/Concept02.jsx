5a390b2e80dc8a60782a42a67bf58ae78c59d203
senalisa <senababacan@hotmail.com>
2024-03-04 18:01:08 +0100

    NOS kaart concept commit 1

--- src/section/Concept02.jsx
+++ src/section/Concept02.jsx
@@ -21,19 +21,20 @@ export function Concept02() {
                 </span>
             </div>
             <div className="my-4 md:my-0">
-                <h3 className="text-2xl md:text-3xl font-semibold text-white">Lorum Ipsum</h3>
+                <h3 className="text-2xl md:text-3xl font-semibold text-white">NOS Kaart</h3>
             </div>
             
             <div className="flex flex-col md:flex-row-reverse md:my-8">
                 <div className="my-4 md:ml-12 md:max-w-[50%]">
                     <p className="mb-4 text-lg leading-6 font-light text-white">
+                    De NOS Kaart is een innovatief voorstel voor een uitbreiding van de bestaande NOS-applicatie. Het hoofddoel is om nieuws voor jongeren op een overzichtelijke manier te presenteren, afgestemd op hun individuele interesses. 
+                    Uit ons doelgroeponderzoek is gebleken dat jongeren nieuws vaak verwarrend vinden en te sterk gericht op politieke en traditionele onderwerpen. 
+                    Met de introductie van de NOS Kaart lossen we dit probleem op door middel van de volgende functionaliteiten.
+                    </p>
+                    {/* <p className="text-lg leading-6 font-light text-white">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
                         ut labore et dolore magna aliqua. Augue eget arcu dictum varius duis. Sodales neque sodales ut etiam sit amet nisl.
-                    </p>
-                    <p className="text-lg leading-6 font-light text-white">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                        ut labore et dolore magna aliqua. Augue eget arcu dictum varius duis. Sodales neque sodales ut etiam sit amet nisl.
-                    </p>
+                    </p> */}
                 </div>
 
                 <div className="my-8 md:my-0 w-full">
@@ -44,14 +45,18 @@ export function Concept02() {
 
 
             <div className="my-4 md:my-16">
+                <h4 className="text-xl leading-3 text-white mb-7 font-bold">Wireframes</h4>
                 <p className="text-lg leading-6 font-light text-white">
+                De NOS Kaart stelt gebruikers in staat om een specifiek onderwerp te kiezen dat hen interesseert, en vervolgens het actuele nieuws van de dag te 
+                verkennen door in en uit te zoomen op een interactieve kaart. Daarnaast bevat de kaart een geavanceerd aanbevelingssysteem dat nieuwsartikelen presenteert 
+                op basis van de individuele interesses van de gebruiker. Hierdoor wordt niet alleen relevant nieuws weergegeven, maar ontvangt de gebruiker ook aanbevelingen 
+                voor algemene nieuwsitems die mogelijk interessant zijn. Daarnaast worden populaire nieuwsberichten ook overzichtelijk gepresenteerd, waardoor de NOS Kaart een 
+                dynamische en gepersonaliseerde nieuwservaring biedt voor jongeren.
+                </p>
+                {/* <p className="text-lg leading-6 font-light text-white">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
                     ut labore et dolore magna aliqua. Augue eget arcu dictum varius duis. Sodales neque sodales ut etiam sit amet nisl.
-                </p>
-                <p className="text-lg leading-6 font-light text-white">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                    ut labore et dolore magna aliqua. Augue eget arcu dictum varius duis. Sodales neque sodales ut etiam sit amet nisl.
-                </p>
+                </p> */}
             </div>
 
             <div className="carousel | flex gap-8 my-8 -mx-8 md:mx-0 overflow-y-hidden">
